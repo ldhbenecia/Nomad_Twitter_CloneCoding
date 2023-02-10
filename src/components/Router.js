cece848c6403ea9d08a2e77beceb9fde116e7f60
DongHyeok Lim <jh07050@gmail.com>
2023-02-11 02:17:11 +0900

    initial setUp
    SetUP

---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+function AppRouter() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn ?
+        <>
+        <Route path="/" element={<Home />} />
+        </>
+         : 
+        <Route path="/" element={<Auth />} />
+        }
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
