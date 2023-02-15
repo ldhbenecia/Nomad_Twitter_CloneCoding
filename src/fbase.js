0e712b56a0deb2f071de7b3903485b0bb7795334
DongHyeok Lim <jh07050@gmail.com>
2023-02-15 22:36:41 +0900

    Feat: Nweeting
    Firebase FireStore: Lweet 클릭 시 데이터베이스에 추가

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,6 +12,8 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-const authService = getAuth(app)
 
-export { app, authService }
+const authService = getAuth(app)
+const dbService = getFirestore(app)
+
+export { app, authService, dbService }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
+import { dbService } from "fbase"
+import { addDoc, collection } from "firebase/firestore"
 
-const Home = () => <span>Home</span>
+const Home = () => {
+  const [lweet, setLweet] = useState("")
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    const docRef = await addDoc(collection(dbService, "lweets"), {
+      lweet,
+      createdAt: Date.now(),
+    })
+    setLweet("")
+  }
+  const onChange = (event) => {
+    setLweet(event.target.value)
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={lweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Lweet" />
+      </form>
+    </div>
+  )
+}
 
 export default Home
