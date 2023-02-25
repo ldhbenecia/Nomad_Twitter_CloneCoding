3fd4d1130850b1e6847f2dcb7b3f1ff0c41c1017
DongHyeok Lim <jh07050@gmail.com>
2023-02-25 22:49:42 +0900

    Feat: Preview Images
    이미지 미리보기 및 미리보기 제거, 첨부파일명 제거 추가

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,25 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { dbService } from "fbase"
-import { addDoc, collection, query, onSnapshot, orderBy } from "firebase/firestore"
+import {
+  addDoc,
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore"
 import Lweet from "components/Lweet"
 
 const Home = ({ userObj }) => {
-  const [lweet, setLweet] = useState("")
+  const [lweet, setLweet] = useState("") // form을 위한 state
   const [lweets, setLweets] = useState([])
+  const [file, setFile] = useState()
 
   useEffect(() => {
     // 실시간으로 데이터를 데이터베이스에서 가져오기
-    const q = query(collection(dbService, "lweets"), orderBy("createdAt", "desc"))
+    const q = query(
+      collection(dbService, "lweets"),
+      orderBy("createdAt", "desc")
+    )
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const nextLweets = querySnapshot.docs.map((doc) => {
         return {
@@ -34,8 +44,29 @@ const Home = ({ userObj }) => {
     })
     setLweet("")
   }
+
   const onChange = (event) => {
     setLweet(event.target.value)
+  }
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event
+    const theFile = files[0]
+    const reader = new FileReader()
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent
+      setFile(result) // onloadend에 finishedEvent의 result를 setFile로 설정
+    }
+    reader.readAsDataURL(theFile)
+  }
+  const fileInput = useRef() // 첨부파일명 삭제하기 위한 훅
+  const onClearFile = () => {
+    setFile(null) // 첨부파일 url 넣는 state 비워서 preview img src 삭제
+    fileInput.current.value = null // 선택했던 첨부파일명 삭제
   }
   return (
     <div>
@@ -47,7 +78,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} ref={fileInput} />
         <input type="submit" value="Lweet" />
+        {file && (
+          <div>
+            <img src={file} width="50px" height="50px" />
+            <button onClick={onClearFile}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {lweets.map((lweet) => (
